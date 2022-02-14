7c51a4bf7417d9ad50e10400ab812a88aa207a86
Aditya Wardianto <adityawardianto.19073@mhs.its.ac.id>
2022-02-14 12:21:15 +0700

    Change InMemoryCache with DiskCache

--- app/api/stocks.py
+++ app/api/stocks.py
@@ -1,21 +1,20 @@
 from dotenv import load_dotenv
 from fastapi import APIRouter, HTTPException
-from fastapi_cache import FastAPICache
-from fastapi_cache.backends.inmemory import InMemoryBackend
-from fastapi_cache.decorator import cache
 from requests import session
 from requests_futures.sessions import FuturesSession
 from ujson import load
 from lxml import html
 from app.utils import *
+from diskcache import Cache
 import os
 
 
 router = APIRouter(prefix="/stocks", tags=["Stocks"])
 
+
 @router.on_event("startup")
 async def startup_stocks():
-    if os.getenv("DEPLOY") is "False":
+    if os.getenv("DEPLOY") == "False":
         load_dotenv()
 
     payload = {
@@ -26,43 +25,48 @@ async def startup_stocks():
     s.post("https://www.dropshipaja.com/page/login_code.php", data=payload)
     router.session = FuturesSession(session=s)
     router.delivery_id = load(open(os.path.join("app", "data", "route_id.json")))
-    FastAPICache.init(InMemoryBackend(), prefix="fastapi-cache")
+    router.cache = Cache()
 
 
 @router.get("/kaos")
-@cache(expire=600,namespace="stock-kaos")
 async def check_stock_kaos():
-    req_list = [
-        router.session.get(
-            "https://www.dropshipaja.com/kategoridesainonline.php?id=6&page=" + str(i)
-        )
-        for i in range(1, 10)
-    ]
-
-    tree_list = [html.fromstring(req.result().content) for req in req_list]
-    stock_list = flat(
-        [
-            tree.cssselect(
-                "h6[class='text-muted stock da-text-secondary-extra-dark mb-2']"
+    router.cache.expire()
+    if "stock_kaos" in list(router.cache):
+        return router.cache.get("stock_kaos", tag="stock")
+    else:
+        req_list = [
+            router.session.get(
+                "https://www.dropshipaja.com/kategoridesainonline.php?id=6&page="
+                + str(i)
             )
-            for tree in tree_list
+            for i in range(1, 10)
         ]
-    )
-    product_name_list = flat(
-        [
-            tree.cssselect("h5[class='text-left text-justify prdktitlenp']")
-            for tree in tree_list
-        ]
-    )
 
-    return dict(
-        zip(
+        tree_list = [html.fromstring(req.result().content) for req in req_list]
+        stock_list = flat(
             [
-                i.text.replace(
-                    "\n                              CUSTOM KAOS ", ""
-                ).replace("                            ", "")
-                for i in product_name_list
-            ],
-            [i.text.replace(" ", "").replace("stock", "") for i in stock_list],
+                tree.cssselect(
+                    "h6[class='text-muted stock da-text-secondary-extra-dark mb-2']"
+                )
+                for tree in tree_list
+            ]
         )
-    )
+        product_name_list = flat(
+            [
+                tree.cssselect("h5[class='text-left text-justify prdktitlenp']")
+                for tree in tree_list
+            ]
+        )
+        out = dict(
+            zip(
+                [
+                    i.text.replace(
+                        "\n                              CUSTOM KAOS ", ""
+                    ).replace("                            ", "")
+                    for i in product_name_list
+                ],
+                [i.text.replace(" ", "").replace("stock", "") for i in stock_list],
+            )
+        )
+        router.cache.set("stock_kaos", out, expire=600, tag="stock")
+        return out
